a0f07116657b18270e68f9a561fd6f25c3df0888
Jatin Singh <singhjatin1812@gmail.com>
2024-10-21 19:00:40 +0530

    Initial Commit

---
+++ Instafilters/InstafiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafiltersApp.swift
+//  Instafilters
+//
+//  Created by Jatin Singh on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
